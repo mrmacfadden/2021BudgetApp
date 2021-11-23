62147fdaf2b3a8ae0c3ddd00602c88377986f0a7
Mr. Macfadden <mmacfadden@glenbrook225.org>
2021-11-23 15:16:55 -0600

    Nov.23

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function findOut(){
+    document.getElementById('panel_1').style.display = 'none';
+    document.getElementById('panel_2').style.display = 'block';
+}
+
+function tryAgain(){
+    document.getElementById('panel_1').style.display = 'block';
+    document.getElementById('panel_2').style.display = 'none';
+}
